5be89dd1d5d1e4887adab5a45f3f4bd5f395570c
stepsen89 <stephanie.senoner@gmail.com>
2020-05-24 20:25:31 +0100

    added basic javascript

---
+++ challenge6-bootcamp-testimonial/main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  const testimonialOne = document.querySelector("#tanya");
+  const testimonialTwo = document.querySelector("#john");
+}
+function nextTestimonial() {
+  console.log("here");
+}
