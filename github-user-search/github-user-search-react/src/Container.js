1752e02cfb1de141e385fe6f025dbf7b2cbb55b8
Steph <stepsen.dev@gmail.com>
2021-10-06 17:06:32 +0100

    changed get user to be in container

--- github-user-search/github-user-search-react/src/Container.js
+++ github-user-search/github-user-search-react/src/Container.js
@@ -1,16 +1,52 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Search from "./Search";
 import UserProfile from "./UserProfile";
+import { getUser } from "./redux/action";
+import { connect } from "react-redux";
 
-class Container extends Component {
-  render() {
-    return (
-      <div class='container'>
-        <Search />
-        <UserProfile />
-      </div>
-    );
-  }
+function Container({ getUser, user }) {
+  // const [user, setUser] = useState({});
+
+  // componentDidMount() {
+  //   const { user } = this.props;
+  //   // console.log(this.props);
+
+  //   if (!user) {
+  //     this.props.getUser("octocat");
+  //     // console.log(user);
+  //     // this.setState({ user });
+  //     console.log("state", this.props.user);
+  //   }
+  // }
+
+  useEffect(() => {
+    getUser("octocat");
+    console.log(user);
+    // console.log(props);
+  }, []);
+
+  // componentDidUpdate(prevProps) {
+  //   if (prevProps.user !== this.props.user) {
+  //     this.setState({ user: this.props.user });
+  //     console.log("props in did update", this.props);
+  //   }
+  // }
+
+  return (
+    <div className='container'>
+      <Search />
+      <UserProfile user={user} />
+    </div>
+  );
 }
 
-export default Container;
+function mapStateToProps(state) {
+  const user = state.user;
+  return { user };
+}
+
+const mapDispatchToProps = {
+  getUser,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Container);
--- github-user-search/github-user-search-react/src/UserProfile.js
+++ github-user-search/github-user-search-react/src/UserProfile.js
@@ -1,10 +1,72 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { ReactComponent as MapMarkerIcon } from "./assets/icon-location.svg";
+import { ReactComponent as WebsiteIcon } from "./assets/icon-website.svg";
+import { ReactComponent as TwitterIcon } from "./assets/icon-twitter.svg";
+import { ReactComponent as CompanyIcon } from "./assets/icon-company.svg";
 
-const UserProfile = () => {
+const UserProfile = ({ user }) => {
+  useEffect(() => {
+    console.log("Hello");
+    console.log(user);
+  }, [user]);
+
+  const getJoinedDate = (d) => {
+    let dDate = new Date(d);
+    let stringDate = dDate.toLocaleString("default", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    });
+
+    return `Joined ${stringDate}`;
+  };
+
   return (
-    <div style={{ background: "green", width: "100%", height: "87%" }}>
-      {" "}
-      Test
+    <div className='card'>
+      <div className='profile'>
+        {console.log(user)}
+        <img src={user.avatar_url} alt='User icon' />
+        <div className='profile-details'>
+          <h3 className='bold'> {user.name} </h3>
+          <p className='profile-details--username'> {user.name} </p>
+          <p> {getJoinedDate(user.created_at)} </p>
+        </div>
+      </div>
+      <div className='description'>
+        <p className='text'> {user.bio || "No description."}</p>
+      </div>
+      <div className='stats'>
+        <div>
+          <p> Repos </p>
+          <p className='bold'> {user.public_repos} </p>
+        </div>
+        <div>
+          <p> Followers </p>
+          <p className='bold'> {user.followers} </p>
+        </div>
+        <div>
+          <p> Following </p>
+          <p className='bold'> {user.following} </p>
+        </div>
+      </div>
+      <div className='address-box'>
+        <div>
+          <MapMarkerIcon className='icon' fill='green' />
+          <span> San Francisco</span>
+        </div>
+        <div>
+          <WebsiteIcon className='icon' />
+          <span> github.blog</span>
+        </div>
+        <div>
+          <TwitterIcon className='icon' />
+          <span> not available</span>
+        </div>
+        <div>
+          <CompanyIcon className='icon' />
+          <span> @github </span>
+        </div>
+      </div>
     </div>
   );
 };
