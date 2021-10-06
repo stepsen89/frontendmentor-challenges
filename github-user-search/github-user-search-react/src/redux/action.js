926159837f677bb9a9b17b18dd4aa53c82e7195b
Steph <stepsen.dev@gmail.com>
2021-10-06 17:07:13 +0100

    added new theme reducer, action start

--- github-user-search/github-user-search-react/src/redux/action.js
+++ github-user-search/github-user-search-react/src/redux/action.js
@@ -1,18 +1,29 @@
 import axios from "axios";
+import { FETCH_USER, SET_THEME } from "./types";
 
 export function getUser(username) {
   return (dispatch) => {
     return axios
       .get(`https://api.github.com/users/${username}`)
       .then((response) => {
-        console.log("test");
+        console.log("res", response);
         dispatch({
-          type: "FETCH_USER",
-          payload: response,
+          type: FETCH_USER,
+          payload: response.data,
         });
       })
       .catch((error) => {
         console.error(error);
       });
+  };
+}
+
+export function setTheme(theme) {
+  console.log("Set theme action");
+  return (dispatch) => {
+    dispatch({
+      type: SET_THEME,
+      payload: theme,
+    });
   };
 }
--- github-user-search/github-user-search-react/src/redux/reducer.js
+++ github-user-search/github-user-search-react/src/redux/reducer.js
@@ -1,14 +1,27 @@
 import { combineReducers } from "redux";
+import { FETCH_USER, SET_THEME } from "./types";
+
 const initialUserState = {
   user: null,
 };
 
+const initialThemeState = {
+  theme: "light",
+};
+
 export function userReducer(state = initialUserState, action) {
   switch (action.type) {
-    case "FETCH_USER":
-      return {
-        user: action.payload,
-      };
+    case FETCH_USER:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export function themeReducer(state = initialThemeState, action) {
+  switch (action.type) {
+    case SET_THEME:
+      return { theme: action.payload };
     default:
       return state;
   }
@@ -16,6 +29,7 @@ export function userReducer(state = initialUserState, action) {
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 export default rootReducer;
