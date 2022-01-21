f2c705dc8b14a4ba1e1d7f176f4ad7ae744330f8
Steph <stepsen.dev@gmail.com>
2022-01-21 15:50:04 +0000

    testimonial grid challenge using tailwindcss

---
+++ testimonial-grid/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: { fontFamily: { sans: "Barlow Semi Condensed, sans-serif" } },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
