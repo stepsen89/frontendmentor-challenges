01a5720e3a4e1b83e312d93022f3ac8a484f7492
Steph <stepsen.dev@gmail.com>
2022-02-20 15:41:16 +0000

    bootcamp testimonial fixes

---
+++ bootcamp-testimonial/main.js
@@ -0,0 +1,7 @@
+document.onload = () => {
+  const testimonialOne = document.querySelector("#tanya");
+  const testimonialTwo = document.querySelector("#john");
+  function nextTestimonial() {
+    testimonialOne.classList.toggle("hidden");
+  }
+};
