c9abe53a58efd4425bb8d8c99edeadb3e1d1d4f9
Steph <stepsen.dev@gmail.com>
2022-02-24 11:20:20 +0000

    memory game: responsive design for tablet and desktop

--- memory-game/src/components/Header.js
+++ memory-game/src/components/Header.js
@@ -58,11 +58,25 @@ function Header({ newGame, resetAll }) {
           {/* <div> */}
           <img src="/images/logo.svg" alt="" />
           {/* </div> */}
-          <div>
+          <div className="mobile">
             <button class="btn small" onClick={showMenuPopup}>
               {" "}
               Menu{" "}
             </button>
+          </div>
+          <div className="tablet">
+            <div>
+              <button className="btn" onClick={reset}>
+                {" "}
+                Restart{" "}
+              </button>
+            </div>
+            <div>
+              <button className="btn gray" onClick={startNewGame}>
+                {" "}
+                New Game{" "}
+              </button>
+            </div>
           </div>
         </div>
       </div>
